0bb4f104fefc862227a7ce6c3fdbf0bdc182f686
allen <yellowd54321@gmail.com>
2022-12-01 20:01:58 +0800

    create SwipeEffect.js

---
+++ src/components/swipeEffect/SwipeEffect.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./swipeEffect.css";
+
+const SwipeEffect = () => {
+  return (
+    <div className="l-swipeEffect">
+      <div className="l-container">
+        <div className="c-content1">
+          <div className="c-content1__child">1</div>
+        </div>
+        <div className="c-content2">
+          <div className="c-content2__child">2</div>
+        </div>
+        <div className="c-content3">
+          <div className="c-content3__child">3</div>
+        </div>
+        <div className="c-content4">
+          <div className="c-content4__child">4</div>
+        </div>
+        <div className="c-content5">
+          <div className="c-content5__child">5</div>
+        </div>
+        <div className="c-content6">
+          <div className="c-content6__child">6</div>
+        </div>
+        <div className="c-content7">
+          <div className="c-content7__child">7</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SwipeEffect;
