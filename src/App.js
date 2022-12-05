a5c60796e9d90e62aa3126d7f15b78841a1f77e4
allen <yellowd54321@gmail.com>
2022-12-05 19:26:54 +0800

    try to use observer in swipe menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SwipeEffect from "./components/swipeEffect/SwipeEffect";
+import SwipeObserver from "./components/swipeObserver/SwipeObserver";
 
 function App() {
   return (
     <div className="App">
-      <SwipeEffect />
+      {/* <SwipeEffect /> */}
+      <SwipeObserver />
     </div>
   );
 }
--- src/components/swipeObserver/SwipeObserver.js
+++ src/components/swipeObserver/SwipeObserver.js
@@ -1,35 +1,69 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./swipeEffect.css";
+import "./swipeObserver.css";
 
-const SwipeEffect = () => {
+const SwipeObserver = () => {
   const KIND_NUMBER = 7;
   // const categoryContentClass = "c-category__content";
   const categoryItemClass = "c-category__item";
   const avtiveCategoryClass = "c-category__item--isActive";
   const contentClass = "l-container__content";
   const containerRef = useRef(null);
-  const content1Ref = useRef(null);
-  const content2Ref = useRef(null);
-  const content3Ref = useRef(null);
-  const content4Ref = useRef(null);
-  const content5Ref = useRef(null);
-  const content6Ref = useRef(null);
-  const content7Ref = useRef(null);
+  //   const content1Ref = useRef(null);
+  //   const content2Ref = useRef(null);
+  //   const content3Ref = useRef(null);
+  //   const content4Ref = useRef(null);
+  //   const content5Ref = useRef(null);
+  //   const content6Ref = useRef(null);
+  //   const content7Ref = useRef(null);
   const [categoryIndex, setCategoryIndex] = useState(0);
+  const contents = useRef([]);
+
+  //   useEffect(() => {
+  // const container = containerRef.current;
+  // const getIndex = () => {
+  //   const containerBound = container.getBoundingClientRect();
+  //   const containerWidth = containerBound.width;
+  //   const content1Bound = content1Ref.current.getBoundingClientRect();
+  //   const content1Left = Math.abs(content1Bound.left);
+  //   const index = Math.floor(content1Left / containerWidth);
+  //   setCategoryIndex(index);
+  // };
+  // container.addEventListener("scroll", getIndex);
+  // return () => container.removeEventListener("scroll", getIndex);
+  //   }, []);
 
   useEffect(() => {
-    const container = containerRef.current;
-    const getIndex = () => {
-      const containerBound = container.getBoundingClientRect();
-      const containerWidth = containerBound.width;
-      const content1Bound = content1Ref.current.getBoundingClientRect();
-      const content1Left = Math.abs(content1Bound.left);
-      const index = Math.floor(content1Left / containerWidth);
-      setCategoryIndex(index);
+    console.log(containerRef.current);
+    let options = {
+      root: containerRef.current,
+      rootMargin: "0px",
+      threshold: 1.0,
     };
-    container.addEventListener("scroll", getIndex);
-    return () => container.removeEventListener("scroll", getIndex);
+
+    let observer = new IntersectionObserver(callback, options);
+    const targets = [];
+    const targetElements = document.getElementsByClassName(
+      "l-container__content"
+    );
+    for (let i = 0; i < targetElements.length; i++) {
+      observer.observe(targetElements[i]);
+      targets.push(targetElements[i]);
+    }
+    contents.current = targets;
   }, []);
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("entry", entry.target);
+        const list = contents.current;
+        const index = list.findIndex((content) => content === entry.target);
+
+        console.log(index);
+        setCategoryIndex(index);
+      }
+    });
+  };
 
   useEffect(() => {
     const categories = document.getElementsByClassName(categoryItemClass);
@@ -56,7 +90,6 @@ const SwipeEffect = () => {
       inline: "center",
     });
   };
-
   const createCategory = () => {
     const categories = [];
     for (let i = 0; i < KIND_NUMBER; i++) {
@@ -85,25 +118,39 @@ const SwipeEffect = () => {
     <div className="l-swipeEffect">
       <div className="l-category">{createCategory()}</div>
       <div className="l-container" ref={containerRef}>
-        <div className="l-container__content c-content1" ref={content1Ref}>
+        <div
+          className="l-container__content c-content1" /* ref={content1Ref} */
+        >
           <div className="c-content1__child">{createProducts(5)}</div>
         </div>
-        <div className="l-container__content c-content2" ref={content2Ref}>
+        <div
+          className="l-container__content c-content2" /* ref={content2Ref} */
+        >
           <div className="c-content2__child">{createProducts(200)}</div>
         </div>
-        <div className="l-container__content c-content3" ref={content3Ref}>
+        <div
+          className="l-container__content c-content3" /* ref={content3Ref} */
+        >
           <div className="c-content3__child">{createProducts(500)}</div>
         </div>
-        <div className="l-container__content c-content4" ref={content4Ref}>
+        <div
+          className="l-container__content c-content4" /* ref={content4Ref} */
+        >
           <div className="c-content4__child">{createProducts(10)}</div>
         </div>
-        <div className="l-container__content c-content5" ref={content5Ref}>
+        <div
+          className="l-container__content c-content5" /* ref={content5Ref} */
+        >
           <div className="c-content5__child">{createProducts(76)}</div>
         </div>
-        <div className="l-container__content c-content6" ref={content6Ref}>
+        <div
+          className="l-container__content c-content6" /* ref={content6Ref} */
+        >
           <div className="c-content6__child">{createProducts(99)}</div>
         </div>
-        <div className="l-container__content c-content7" ref={content7Ref}>
+        <div
+          className="l-container__content c-content7" /* ref={content7Ref} */
+        >
           <div className="c-content7__child">{createProducts(300)}</div>
         </div>
       </div>
@@ -111,4 +158,4 @@ const SwipeEffect = () => {
   );
 };
 
-export default SwipeEffect;
+export default SwipeObserver;
