ae880dfe1ede20e3659c67cbec4af9fc9f60a13f
allen <yellowd54321@gmail.com>
2023-01-18 15:47:35 +0800

    整合原先的檔案

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import SwipeEffect from "./components/swipeEffect/SwipeEffect";
 import SwipeObserver from "./components/swipeObserver/SwipeObserver";
 import ApiTest from "./components/apiTest/ApiTest";
+import TapPay from "./components/tapPay/TapPay";
 
 function App() {
   return (
     <div className="App">
       {/* <SwipeEffect /> */}
       {/* <SwipeObserver /> */}
-      <ApiTest />
+      <TapPay />
+      {/* <ApiTest /> */}
     </div>
   );
 }
--- src/components/apiTest/ApiTest.js
+++ src/components/apiTest/ApiTest.js
@@ -1,6 +1,14 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
+// const BASE_URL = "http://192.57.140.250/QPay/BackendURL/";
+// const BASE_URL = "http://60.248.161.72/QPay/BackendURL/";
+// const data = {
+//   CMP_NO: "ODBC0000",
+//   PUR_NO: "BU0A7030001",
+//   SERVER: "60.248.161.72",
+// };
+
 const BASE_URL = "http://localhost:8080/user/allen";
 
 const ApiTest = () => {
