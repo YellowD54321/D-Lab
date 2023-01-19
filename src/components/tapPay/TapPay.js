e7b8ae4430cc5d57e0b06b2134ba9c03a9046026
allen <yellowd54321@gmail.com>
2023-01-19 14:36:37 +0800

    試做以視窗開啟信用卡畫面

--- src/components/tapPay/TapPay.js
+++ src/components/tapPay/TapPay.js
@@ -1,124 +1,126 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./tapPay.css";
 import axios from "axios";
+import TappayWindow from "./TappayWindow";
+import { Dialog } from "@mui/material";
 
-const BASE_URL = "http://localhost:8080/pay-by-prime";
+const APP_ID = 126903;
+const APP_KEY =
+  "app_A2Tc5Hb6sekIkziqXsZf9fit0C7X7shLeuc0onPTeW8f9ZvFmAoFCk8QhjpL";
 
 const TapPay = () => {
-  useEffect(() => {
-    window.TPDirect.setupSDK(
-      126903,
-      "app_A2Tc5Hb6sekIkziqXsZf9fit0C7X7shLeuc0onPTeW8f9ZvFmAoFCk8QhjpL",
-      "sandbox"
-    );
-    const fields = {
-      number: {
-        // css selector
-        element: "#card-number",
-        placeholder: "**** **** **** ****",
-      },
-      expirationDate: {
-        // DOM object
-        element: document.getElementById("card-expiration-date"),
-        placeholder: "MM / YY",
-      },
-      ccv: {
-        element: "#card-ccv",
-        placeholder: "後三碼",
-      },
-    };
-    window.TPDirect.card.setup({
-      fields: fields,
-      styles: {
-        // Style all elements
-        input: {
-          color: "gray",
-        },
-        // Styling ccv field
-        "input.ccv": {
-          "font-size": "16px",
-        },
-        // Styling expiration-date field
-        "input.expiration-date": {
-          "font-size": "16px",
-        },
-        // Styling card-number field
-        "input.card-number": {
-          "font-size": "16px",
-        },
-        // style focus state
-        ":focus": {
-          color: "black",
-        },
-        // style valid state
-        ".valid": {
-          color: "green",
-        },
-        // style invalid state
-        ".invalid": {
-          color: "red",
-        },
-        // Media queries
-        // Note that these apply to the iframe, not the root window.
-        "@media screen and (max-width: 400px)": {
-          input: {
-            color: "orange",
-          },
-        },
-      },
-      // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
-      isMaskCreditCardNumber: true,
-      maskCreditCardNumberRange: {
-        beginIndex: 6,
-        endIndex: 11,
-      },
-    });
+  const [isWindowOpen, setIsWindowOpen] = useState(false);
+  // useEffect(() => {
+  //   window.TPDirect.setupSDK(APP_ID, APP_KEY, "sandbox");
+  //   const fields = {
+  //     number: {
+  //       // css selector
+  //       element: "#card-number",
+  //       placeholder: "**** **** **** ****",
+  //     },
+  //     expirationDate: {
+  //       // DOM object
+  //       element: document.getElementById("card-expiration-date"),
+  //       placeholder: "MM / YY",
+  //     },
+  //     ccv: {
+  //       element: "#card-ccv",
+  //       placeholder: "後三碼",
+  //     },
+  //   };
+  //   window.TPDirect.card.setup({
+  //     fields: fields,
+  //     styles: {
+  //       // Style all elements
+  //       input: {
+  //         color: "gray",
+  //       },
+  //       // Styling ccv field
+  //       "input.ccv": {
+  //         "font-size": "16px",
+  //       },
+  //       // Styling expiration-date field
+  //       "input.expiration-date": {
+  //         "font-size": "16px",
+  //       },
+  //       // Styling card-number field
+  //       "input.card-number": {
+  //         "font-size": "16px",
+  //       },
+  //       // style focus state
+  //       ":focus": {
+  //         color: "black",
+  //       },
+  //       // style valid state
+  //       ".valid": {
+  //         color: "green",
+  //       },
+  //       // style invalid state
+  //       ".invalid": {
+  //         color: "red",
+  //       },
+  //       // Media queries
+  //       // Note that these apply to the iframe, not the root window.
+  //       "@media screen and (max-width: 400px)": {
+  //         input: {
+  //           color: "orange",
+  //         },
+  //       },
+  //     },
+  //     // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
+  //     isMaskCreditCardNumber: true,
+  //     maskCreditCardNumberRange: {
+  //       beginIndex: 6,
+  //       endIndex: 11,
+  //     },
+  //   });
 
-    window.TPDirect.card.onUpdate(function (update) {
-      // update.canGetPrime === true
-      // --> you can call TPDirect.card.getPrime()
-      if (update.canGetPrime) {
-        console.log("update.canGetPrime");
-        // Enable submit Button to get prime.
-        // submitButton.removeAttribute('disabled')
-      } else {
-        // Disable submit Button to get prime.
-        // submitButton.setAttribute('disabled', true)
-      }
+  //   window.TPDirect.card.onUpdate(function (update) {
+  //     // update.canGetPrime === true
+  //     // --> you can call TPDirect.card.getPrime()
+  //     if (update.canGetPrime) {
+  //       console.log("update.canGetPrime");
+  //       // Enable submit Button to get prime.
+  //       // submitButton.removeAttribute('disabled')
+  //     } else {
+  //       // Disable submit Button to get prime.
+  //       // submitButton.setAttribute('disabled', true)
+  //     }
 
-      // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unionpay','unknown']
-      if (update.cardType === "visa") {
-        // Handle card type visa.
-      }
+  //     // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unionpay','unknown']
+  //     if (update.cardType === "visa") {
+  //       // Handle card type visa.
+  //     }
 
-      // number 欄位是錯誤的
-      if (update.status.number === 2) {
-        // setNumberFormGroupToError()
-      } else if (update.status.number === 0) {
-        // setNumberFormGroupToSuccess()
-      } else {
-        // setNumberFormGroupToNormal()
-      }
+  //     // number 欄位是錯誤的
+  //     if (update.status.number === 2) {
+  //       // setNumberFormGroupToError()
+  //     } else if (update.status.number === 0) {
+  //       // setNumberFormGroupToSuccess()
+  //     } else {
+  //       // setNumberFormGroupToNormal()
+  //     }
 
-      if (update.status.expiry === 2) {
-        // setNumberFormGroupToError()
-      } else if (update.status.expiry === 0) {
-        // setNumberFormGroupToSuccess()
-      } else {
-        // setNumberFormGroupToNormal()
-      }
+  //     if (update.status.expiry === 2) {
+  //       // setNumberFormGroupToError()
+  //     } else if (update.status.expiry === 0) {
+  //       // setNumberFormGroupToSuccess()
+  //     } else {
+  //       // setNumberFormGroupToNormal()
+  //     }
 
-      if (update.status.ccv === 2) {
-        // setNumberFormGroupToError()
-      } else if (update.status.ccv === 0) {
-        // setNumberFormGroupToSuccess()
-      } else {
-        // setNumberFormGroupToNormal()
-      }
-    });
-  }, []);
+  //     if (update.status.ccv === 2) {
+  //       // setNumberFormGroupToError()
+  //     } else if (update.status.ccv === 0) {
+  //       // setNumberFormGroupToSuccess()
+  //     } else {
+  //       // setNumberFormGroupToNormal()
+  //     }
+  //   });
+  // }, []);
 
   const handleClickSubmitButton = () => {
+    const BASE_URL = "http://localhost:8080/pay-by-prime";
     // 取得 TapPay Fields 的 status
     const tappayStatus = window.TPDirect.card.getTappayFieldsStatus();
     console.log("tappayStatus", tappayStatus);
@@ -152,9 +154,20 @@ const TapPay = () => {
 
   return (
     <div className="tappay-page">
-      <div className="tpfield" id="card-number"></div>
+      {/* <div className="tpfield" id="card-number"></div>
       <div className="tpfield" id="card-expiration-date"></div>
-      <div className="tpfield" id="card-ccv"></div>
+      <div className="tpfield" id="card-ccv"></div> */}
+      <TappayWindow
+        isWindowOpen={isWindowOpen}
+        setIsWindowOpen={setIsWindowOpen}
+      />
+      <button
+        onClick={() => {
+          setIsWindowOpen((pre) => !pre);
+        }}
+      >
+        Open Card Window
+      </button>
       <button onClick={handleClickSubmitButton}>Submit</button>
       <button onClick={handleClickPayByCardTokenButton}>
         Pay By Card Token
---
+++ src/components/tapPay/TappayWindow.js
@@ -0,0 +1,144 @@
+import { Dialog } from "@mui/material";
+import React, { useEffect, useRef } from "react";
+
+const APP_ID = 126903;
+const APP_KEY =
+  "app_A2Tc5Hb6sekIkziqXsZf9fit0C7X7shLeuc0onPTeW8f9ZvFmAoFCk8QhjpL";
+
+const hideClass = "is-hide";
+
+const TappayWindow = ({ isWindowOpen, setIsWindowOpen }) => {
+  const windowRef = useRef();
+
+  useEffect(() => {
+    console.log("window first render");
+    window.TPDirect.setupSDK(APP_ID, APP_KEY, "sandbox");
+    const fields = {
+      number: {
+        // css selector
+        element: "#card-number",
+        placeholder: "**** **** **** ****",
+      },
+      expirationDate: {
+        // DOM object
+        element: document.getElementById("card-expiration-date"),
+        placeholder: "MM / YY",
+      },
+      ccv: {
+        element: "#card-ccv",
+        placeholder: "後三碼",
+      },
+    };
+    window.TPDirect.card.setup({
+      fields: fields,
+      styles: {
+        // Style all elements
+        input: {
+          color: "gray",
+        },
+        // Styling ccv field
+        "input.ccv": {
+          "font-size": "16px",
+        },
+        // Styling expiration-date field
+        "input.expiration-date": {
+          "font-size": "16px",
+        },
+        // Styling card-number field
+        "input.card-number": {
+          "font-size": "16px",
+        },
+        // style focus state
+        ":focus": {
+          color: "black",
+        },
+        // style valid state
+        ".valid": {
+          color: "green",
+        },
+        // style invalid state
+        ".invalid": {
+          color: "red",
+        },
+        // Media queries
+        // Note that these apply to the iframe, not the root window.
+        "@media screen and (max-width: 400px)": {
+          input: {
+            color: "orange",
+          },
+        },
+      },
+      // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
+      isMaskCreditCardNumber: true,
+      maskCreditCardNumberRange: {
+        beginIndex: 6,
+        endIndex: 11,
+      },
+    });
+
+    window.TPDirect.card.onUpdate(function (update) {
+      // update.canGetPrime === true
+      // --> you can call TPDirect.card.getPrime()
+      if (update.canGetPrime) {
+        console.log("update.canGetPrime");
+        // Enable submit Button to get prime.
+        // submitButton.removeAttribute('disabled')
+      } else {
+        // Disable submit Button to get prime.
+        // submitButton.setAttribute('disabled', true)
+      }
+
+      // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unionpay','unknown']
+      if (update.cardType === "visa") {
+        // Handle card type visa.
+      }
+
+      // number 欄位是錯誤的
+      if (update.status.number === 2) {
+        // setNumberFormGroupToError()
+      } else if (update.status.number === 0) {
+        // setNumberFormGroupToSuccess()
+      } else {
+        // setNumberFormGroupToNormal()
+      }
+
+      if (update.status.expiry === 2) {
+        // setNumberFormGroupToError()
+      } else if (update.status.expiry === 0) {
+        // setNumberFormGroupToSuccess()
+      } else {
+        // setNumberFormGroupToNormal()
+      }
+
+      if (update.status.ccv === 2) {
+        // setNumberFormGroupToError()
+      } else if (update.status.ccv === 0) {
+        // setNumberFormGroupToSuccess()
+      } else {
+        // setNumberFormGroupToNormal()
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const isHide = !isWindowOpen;
+    if (!windowRef.current) {
+      return;
+    }
+    if (isHide) {
+      windowRef.current.classList.add(hideClass);
+    } else {
+      windowRef.current.classList.remove(hideClass);
+    }
+  }, [isWindowOpen]);
+
+  return (
+    <div className="tappay-window" ref={windowRef}>
+      <div className="tpfield" id="card-number"></div>
+      <div className="tpfield" id="card-expiration-date"></div>
+      <div className="tpfield" id="card-ccv"></div>
+    </div>
+  );
+};
+
+export default TappayWindow;
